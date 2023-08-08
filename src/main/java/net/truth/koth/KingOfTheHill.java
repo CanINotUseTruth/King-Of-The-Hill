5ec5d4a3f3881f3346b252a7025c8aa63c03dc70
Truth <tobyrutherfordfb@gmail.com>
2023-08-08 20:13:03 +1000

    Initial Commit V2

---
+++ src/main/java/net/truth/koth/KingOfTheHill.java
@@ -0,0 +1,16 @@
+package net.truth.koth;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Very important comment
+public class KingOfTheHill implements ModInitializer {
+	public static final String MOD_ID = "koth";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/truth/koth/KingOfTheHillClient.java
@@ -0,0 +1,10 @@
+package net.truth.koth;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KingOfTheHillClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
